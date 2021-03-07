9caeea5a570ee8430992ed6d0b3fbf91e92f2d83
lim-mil <limyel@outlook.com>
2021-03-07 15:17:32 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <NavBar></NavBar>
     <div class="columns mt-5 content">
-      <div class="column is-6 is-offset-2">
+      <div class="column is-7 is-offset-1">
         <router-view/>
       </div>
-      <div class="column is-2">
+      <div class="column is-3">
         <SideBar></SideBar>
       </div>
     </div>
